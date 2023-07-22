389e513b4277fdeb1c235367a4471518d90e48a3
Umang Saxena <hanusaxena68@gmail.com>
2023-07-22 14:38:51 +0530

    Made Some New Components

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Newsitems from './Newsitems'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        News Component
+        <Newsitems/>
+      </div>
+    )
+  }
+}
