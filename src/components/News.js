a1dc6e2afbaa4e49e92c4fe97229a09ba5f1e20e
Umang Saxena <hanusaxena68@gmail.com>
2023-07-22 21:44:16 +0530

    Got api end-point and got sample js

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,20 @@ import Newsitems from './Newsitems'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        News Component
-        <Newsitems/>
+      <div className="container my-3">
+        <h2>Welcome to News Leopards</h2>
+        <div className="row">
+            <div className="col-md-4">
+                <Newsitems title="myTitle" description="myDesc" />
+            </div>
+            <div className="col-md-4">
+                <Newsitems title="myTitle" description="myDesc" />
+            </div>
+            <div className="col-md-4">
+                <Newsitems title="myTitle" description="myDesc" />
+            </div>
+        </div>
+
       </div>
     )
   }
