8dc4b8a323c087eb7d344f4dbc30dfa2c4304be1
Umang Saxena <hanusaxena68@gmail.com>
2023-07-23 10:25:16 +0530

    State and Props Concept done in class comnponent

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <div >
-        <Navbar/>
+        <Navbar title={"My Title"} description={"My Desc"} imageUrl={"https://ichef.bbci.co.uk/news/1024/branded_news/14F1F/production/_130219758_sofiatastingcrickettagliatelle.jpg"} />
         <News/>
       </div>
     )
--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 
 export default class Newsitems extends Component {
+    //  let {title,description,imageUrl} = this.props;
     render() {
         return (
-            <div>
+            <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="..." className="card-img-top" alt="..."/>
+                    <img src={this.props.imageUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title text-center">{this.props.title}</h5>
+                            <p className="card-text">{this.props.description}</p>
+                            <a href="#" className="btn btn-sm btn-primary">Read More</a>
                         </div>
                 </div>
             </div>
