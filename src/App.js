ce3ecdc23e56ef9f168e4adc82c787b90342f214
Umang Saxena <hanusaxena68@gmail.com>
2023-07-24 23:39:20 +0530

    Displayed only 6 news per page using props

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default class App extends Component {
   render() {
     return (
       <div >
-        <Navbar title={"My Title"} description={"My Desc"} imageUrl={"https://ichef.bbci.co.uk/news/1024/branded_news/14F1F/production/_130219758_sofiatastingcrickettagliatelle.jpg"} />
-        <News/>
+        <Navbar/>
+        <News pageSize={6} />
       </div>
     )
   }
